5e56da4996c392c49daf307c914da015f2d3daee
Aa298 <75563393+Aa298@users.noreply.github.com>
2021-04-29 08:48:29 -0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")
+    textSize(15)
+    text("ETADASBA", 700,150)
+    fill("lightblue")
+    text("Hint: Where we Store Data !!", 700,170)
+
+     
+    fill("white")
+    textSize(15)
+    text("cutnifon", 100,250)
+    fill("lightblue")
+    text("Hint: WHAT WE CALL Before Writing THE CoDe!!", 100,270)
+
+
+
+}
